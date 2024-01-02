b8289e3076a4fa5c1994db79bf29616232bc6144
Александр <148478282+Arkserks@users.noreply.github.com>
2024-01-02 11:20:33 +0600

    Create 21.9.1.py

---
+++ Small Python Tasks/21.9.1.py
@@ -0,0 +1,15 @@
+"""Создайте метод, который возвращает атрибуты прямоугольника как строку ( постарайтесь применить магический метод __str__).
+Для объекта прямоугольника со значениями атрибута x = 5, y = 10, width = 50, height = 100 метод должен вернуть строку Rectangle : 5, 10, 50, 100."""
+
+class Rectangle:
+    def __init__(self, x,y, width,heigth):
+        self.x = x
+        self.y = y
+        self.width=width
+        self.heigth=heigth
+ 
+    def __str__(self):
+        return f'Rectangle : {self.x}, {self.y}, {self.width}, {self.heigth}.'
+ 
+rect_1=Rectangle(5,10,50,100)
+print(rect_1)
